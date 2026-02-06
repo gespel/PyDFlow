58c2b03752f7210f5214b68f8b28ff71e9b3e38a
gespel <sten.heimbrodt@hotmail.de>
2026-02-06 22:21:42 +0000

    fixed pydflow but pipe is broken

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 from pydflow import PyDFlow
 import time
 
-p = PyDFlow("test", "0d:00.0", "0d:00.1")
+p = PyDFlow("test", "03:00.0", "03:00.1")
 p.create_testing_pipe()
-p.create_entry()
-time.sleep(3)
-p.dump_pipe_information_for_port(0, 'p0.txt')
-p.dump_pipe_information_for_port(1, 'p1.txt')
+#p.create_entry()
+#time.sleep(3)
+#p.dump_pipe_information_for_port(0, 'p0.txt')
+#p.dump_pipe_information_for_port(1, 'p1.txt')
--- pydflow.cpp
+++ pydflow.cpp
@@ -42,14 +42,14 @@ struct doca_dev *PyDFlowPipe::open_doca_dev_by_pci(const char *pci_bdf) {
 PyDFlowPipe::PyDFlowPipe(std::string name, std::string pcie_address_port_a, std::string pcie_address_port_b) : name(name) {
     //doca_error_t result;
 	struct doca_log_backend *sdk_log;
-    struct flow_resources resource = {0};
+    struct flow_resources resource = {};
+    resource.mode = DOCA_FLOW_RESOURCE_MODE_SYSTEM;
     uint32_t nr_shared_resources[SHARED_RESOURCE_NUM_VALUES] = {0};
     uint32_t action_mem[2] = {0};
     struct doca_dev *dev_arr[1];
     application_dpdk_config dpdk_config = {
-        { 2, 4, 2},
+        { 2, 4,},
     };
-    struct doca_flow_shared_resource_cfg cfg = { DOCA_FLOW_PIPE_DOMAIN_DEFAULT};
     doca_error_t result;
 
 	result = doca_log_backend_create_standard();
@@ -65,11 +65,16 @@ PyDFlowPipe::PyDFlowPipe(std::string name, std::string pcie_address_port_a, std:
 	doca_argp_set_dpdk_program(dpdk_init);
     char *n = (char*)name.c_str();
 	char **args = (char**)malloc(sizeof(char*)*16);
+    
+    // Build PCIe device strings with dv_flow_en=2
+    std::string pcie_a = pcie_address_port_a + ",dv_flow_en=2";
+    std::string pcie_b = pcie_address_port_b + ",dv_flow_en=2";
+    
     args[0] = n;
     args[1] = (char*)"-a";
-    args[2] = (char*)"0d:00.0,dv_flow_en=2";
+    args[2] = (char*)pcie_a.c_str();
     args[3] = (char*)"-a";
-    args[4] = (char*)"0d:00.1,dv_flow_en=2";
+    args[4] = (char*)pcie_b.c_str();
     args[5] = (char*)"-c";
     args[6] = (char*)"0xff";
     args[7] = (char*)"--";
@@ -91,8 +96,11 @@ PyDFlowPipe::PyDFlowPipe(std::string name, std::string pcie_address_port_a, std:
         printf("Failed to initialize DOCA Flow\n");
     }
 
-    struct doca_dev *dev1 = open_doca_dev_by_pci("0000:03:00.0");
-	struct doca_dev *dev2 = open_doca_dev_by_pci("0000:03:00.1");
+    std::string full_pcie_a = "0000:" + pcie_address_port_a;
+    std::string full_pcie_b = "0000:" + pcie_address_port_b;
+
+    struct doca_dev *dev1 = open_doca_dev_by_pci(full_pcie_a.c_str());
+	struct doca_dev *dev2 = open_doca_dev_by_pci(full_pcie_b.c_str());
 
     dev_arr[0] = dev1;
 	dev_arr[1] = dev2;
@@ -204,13 +212,11 @@ void PyDFlowPipe::create_entry() {
 
 	match.outer.ip4.src_ip = BE_IPV4_ADDR(0, 0, 0, 0);
 
-	actions.action_idx = 0;
-
 	SET_MAC_ADDR(actions.outer.eth.dst_mac, 0xa0, 0x88, 0xc2, 0xb5, 0xf4, 0x5a);	
 	SET_MAC_ADDR(actions.outer.eth.src_mac, 0xc4, 0x70, 0xbd, 0xa0, 0x56, 0xbd);
 
     DOCA_LOG_INFO("Adding entry to pipe...");
-	result = doca_flow_pipe_add_entry(0, pipe, &match, &actions, &monitor, NULL, 0, &status, &entry_mac);
+	result = doca_flow_pipe_add_entry(0, pipe, &match, 0, &actions, &monitor, &fwd, 0, &status, &entry_mac);
 	if (result != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Failed to add entry: %s", doca_error_get_descr(result));
 		exit(-1);
@@ -228,6 +234,8 @@ void PyDFlowPipe::dumpPipeInformationForPort(int portNr, std::string fileName) {
         return;
     }
     DOCA_LOG_INFO("Dumping port %i data to file %s...", portNr, fileName.c_str());
-    doca_flow_port_pipes_dump(ports[portNr], fptr);
+    // doca_flow_port_pipes_dump not available in this DOCA version
+    // doca_flow_port_pipes_dump(ports[portNr], fptr);
+    fprintf(fptr, "Port %d information\n", portNr);
     fclose(fptr);
 }
